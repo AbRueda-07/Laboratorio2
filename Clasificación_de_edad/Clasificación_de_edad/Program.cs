754ecbb1c879ae046c4478ef5d8a8b882394ddf1
AbRueda-07 <aruedaa@miumg.edu.gt>
2025-02-14 00:59:45 -0600

    Agregué más documentos de la carperta laboratorio2-program

---
+++ Clasificación_de_edad/Clasificación_de_edad/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Clasificación_de_edad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su edad: ");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad < 12)
+            {
+                Console.WriteLine("Niño");
+            }
+            else if (edad >= 12 && edad <= 17)
+            {
+                Console.WriteLine("Es un Adolescente");
+            }
+            else if (edad >= 18 && edad <= 59)
+            {
+                Console.WriteLine("Es un Adulto");
+       
+            }
+            else
+            {
+                Console.WriteLine("Es un adulto mayor");
+            }
+        }
+    }
+}
---
+++ Verificación_de_Acceso/Verificación_de_Acceso/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Verificación_de_Acceso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombrecorrecto = "Usuario";
+            String contraseñacorrecta = "102030";
+
+            Console.Write("Ingrese su nombre de usuario:");
+            string nombreUsuario = Console.ReadLine();
+
+            Console.Write("Ingrese su contraseña:");
+            string contrasena = Console.ReadLine();
+
+            if (nombreUsuario == nombrecorrecto && contrasena == contraseñacorrecta)
+            {
+                Console.WriteLine("Acceso Concedido");
+            }
+            else if (nombreUsuario  == nombrecorrecto)
+            {
+                Console.WriteLine("Contraseña incorrecta");
+            }
+            else
+            {
+                Console.WriteLine("Usuario no registrado");
+            }
+        }
+    }
+}
